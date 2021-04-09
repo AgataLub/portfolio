471d9a6b8eb568e111a58c92560b7052ce0b618c
Agata <agat0538@stud.kea.dk>
2021-04-09 16:14:04 +0200

    sidebar animation

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 "use strict";
 
+window.addEventListener("load", launchEffects);
+
+let sentence;
+let childrenLetters;
+let id;
+let paragraph;
+
+function launchEffects() {
+  let menuitems = document.querySelectorAll(".menuitem");
+
+  for (var i = 0; i < menuitems.length; i++) {
+    menuitems[i].addEventListener("mouseenter", getText);
+  }
+}
+
+function getText() {
+  sentence = this.textContent;
+  id = sentence;
+  sentence = sentence.split("");
+  console.log("getText: " + sentence);
+
+  paragraph = document.getElementById(id);
+  paragraph.innerHTML = "";
+
+  sentence.map(createSeparateLetter);
+
+  let children = paragraph.childNodes;
+
+  for (var i = 0; i < children.length; i++) {
+    console.log("paragraph " + i);
+    children[i].style.setProperty("animation-delay", i * 0.05 + "s");
+  }
+}
+
+function createSeparateLetter(letter) {
+  console.log("createSeparateLetter " + letter);
+
+  paragraph.innerHTML += "<span class=letter>" + letter + "</span>";
+}
+
 $(document).mousemove(function (event) {
   let windowWidth = $(window).width();
   let windowHeight = $(window).height();
